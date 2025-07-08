e9cf62f11f8c37ca4a35ca03a1e982aa43ffcb8c
Yusuf Ateş <875yusufates@gmail.com>
2025-07-08 10:05:01 +0300

    Initial commit

---
+++ src/main/java/com/example/library/entities/Student.java
@@ -0,0 +1,21 @@
+package com.example.library.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Data
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+
+
+}
